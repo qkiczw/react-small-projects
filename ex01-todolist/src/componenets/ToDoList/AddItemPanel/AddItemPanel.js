299e65556244c30663f1c9471ee46b281dc9dd12
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-01-03 17:43:05 +0100

    I added icon to add button a start styling add task form

--- ex01-todolist/src/componenets/ToDoList/AddItemPanel/AddItemPanel.js
+++ ex01-todolist/src/componenets/ToDoList/AddItemPanel/AddItemPanel.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import styles from './AddItemPanel.module.css';
+import addTaskIcon from '../../../assets/delete.png';
 
 const AddItemPanel = (props)=> (
-    <>
-        <p>This pannel will add taks to the state!</p>
-        <form onSubmit={props.addItem}>
-            <input name='taskName' placeholder='Enter new Task'></input>
-            <select name='taskPriority'>
-                <option value='important'>Important</option>
-                <option value='normal'>Normal</option>
-                <option value='lowPriority'>Low Priority</option>
-            </select>
-            <button type='submit'>+ Add</button>
-        </form>
-    </>
+    <form onSubmit={props.addItem} className={styles.wrapper}>
+        <input id='taskName' name='taskName' placeholder='Enter new Task' className={styles.inputField}/>
+        <select id='priority' name='taskPriority' className={styles.inputField}>
+            <option value='important'>Important</option>
+            <option value='normal'>Normal</option>
+            <option value='lowPriority'>Low Priority</option>
+        </select>
+        <button type='submit' className={styles.btn}><img src={addTaskIcon} className={styles.icon} alt='Add Task Icon'/></button>
+    </form>
 )
 
 export default AddItemPanel;
