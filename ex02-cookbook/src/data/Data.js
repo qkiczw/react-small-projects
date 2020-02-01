2dd8e2c77fa9cf95cce445fe95f130ef21e76081
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2020-02-01 16:22:06 +0100

    I just added some data

--- ex02-cookbook/src/data/Data.js
+++ ex02-cookbook/src/data/Data.js
@@ -23,11 +23,104 @@ const data = {
             steps: '',
             difficulty: 2,
             image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/placki-owsiane.jpg',
+        },
+        {
+            id: 3,
+            category: 'Śniadanie',
+            title: 'Francuskie tosty serowe',
+            description: 'Jakiś opis tu musi być',
+            ingredients: ['pieczywo tostowe 6szt.', 'Rama', 'Delikat przyprawa uniwersalna Knorr 3 łyżki', 'jajko 2szt.', 'ser żółty 80g', 'natka pietruszki 0,5 pęczka'],
+            time: 15,
+            steps: '',
+            difficulty: 1,
+            image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/francuskie-tosty-serowe.jpg',
+        },
+        {
+            id: 4,
+            category: 'Śniadanie',
+            title: 'Omlet z kurczakiem i brokułami',
+            description: 'Jakiś opis tu musi być',
+            ingredients: ['filet z kurczaka 1szt.', 'Przyprawa do kurczaka Knorr 1 opakowanie', 'mały brokuł 1szt.', 'jajko 10szt.', 'oliwa z oliwek', 'masło'],
+            time: 30,
+            steps: '',
+            difficulty: 1,
+            image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/omlet-z-kurczakiem-i-brokulami592132.jpg',
+        },
+        {
+            id: 5,
+            category: 'Śniadanie',
+            title: 'Jajka faszerowane szpinakiem',
+            description: 'Jakiś opis tu musi być',
+            ingredients: ['jajka 12szt.', 'Bulion na włoszczyźnie Knorr 1szt.', 'swieży szpinak 200g', 'starty ser żółty 3 łyżki', 'Papryka słodka z Hiszpanii Knorr 1 łyżeczka', 'Gałka muszkatołowa z Indonezji Knorr 0,5 łyżeczki', 'masło 20g', 'kiełki 1 łyżeczka'],
+            time: 30,
+            steps: '',
+            difficulty: 1,
+            image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/jaja-faszerowane-szpinakiem-2.jpg'
         }
+        ],
+    dinner: [
+        {
+            id: 1,
+            category: 'Obiad',
+            title: 'Makaron w sosie pieczarkowym',
+            description: 'Jakiś opis tu musi być',
+            ingredients: ['filety z piersi kurczaka 500g', 'Fix Świderki z kurczakiem w sosie serowym Knorr 1 opakowanie', 'makaron tagliatelle 250g', 'pieczarki 500g', 'mleko 150ml', 'cebula 1szt.', 'musztarda 2 łyżki', 'świeża natka pietruszki 2 łyżki', 'olej rzepakowy 1 łyżka'],
+            time: 20,
+            steps: '',
+            difficulty: 1,
+            image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/cheesy-chicken-and-pasta_a9727.jpg'
+        },
+        {
+            id: 2,
+            category: 'Obiad',
+            title: 'Pomidorowa z makaronem',
+            description: 'Jakiś opis tu musi być',
+            ingredients: ['pomidory z puszki bez skóry 400g', 'Rosół z kury Knorr 2szt.', 'makaron np. świderki 150g', 'cebula 1szt.', 'ząbek czosnku 2szt.', 'natka pietruszki lub koperek 1 łyżka', 'oliwa 1 łyżka'],
+            time: 90,
+            steps: '',
+            difficulty: 2,
+            image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/tomato-soup_a85673.jpg'
+        },
     ],
-    dinner: [],
-    supper: []
+    supper: [
+        {
+            id: 1,
+            category: 'Kolacja',
+            title: '',
+            description: 'Ciasto na słodkie naleśniki',
+            ingredients: ['mąka pszenna 1 szklanka', 'jajka 2szt.', 'mleko 0,5 szklanki', 'woda mineralna 0,5 szklanki', 'cukier waniliowy 1 łyżka', 'miód 1 łyżka', 'olej 30ml', 'sół 1 szczypta'],
+            time: 15,
+            steps: '',
+            difficulty: 1,
+            image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/ciasto-nalesnikowe-na-slodko.jpg'
+        },
+        {
+            id: 2,
+            category: 'Kolacja',
+            title: 'Kapusta pekińska z jajkiem i kukurydzą',
+            description: 'Jakiś opis tu musi być',
+            ingredients: ['kapusta pekińska 1szt.', 'Sos sałatkowy czosnkowy Knorr 1 opakowanie', 'papryka czerwona 1 sztuka', 'kukurydza 1 opkaowanie', 'jajka na twardo 3 sztuki', 'pieczarki konserwowe 1 opakowanie', 'Majonez Hellmanns Babuni 4 łyżki', 'natka pietruszki do dekoracji'],
+            time: 15,
+            steps: '',
+            difficulty: 1,
+            image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/kapusta-pekinska-z-jajkiem-i-kukurydza.jpg'
+        },
+    ]
 }
 
 
+// object sample
+/*
+{
+    id: 1,
+    category: 'Obiad',
+    title: '',
+    description: 'Jakiś opis tu musi być',
+    ingredients: [],
+    time: 20,
+    steps: '',
+    difficulty: 1,
+    image: 'https://s3.przepisy.pl/przepisy3ii/img/variants/670x0/cheesy-chicken-and-pasta_a9727.jpg'
+},
+*/
 export default data;
